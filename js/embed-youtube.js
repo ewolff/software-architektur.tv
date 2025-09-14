2e9a6e962fb734a3f7eaf36d5d70d805f6fb133a
Eberhard Wolff <eberhard.wolff@posteo.net>
2025-09-14 13:23:36 +0200

    Identify the YouTube container by id

--- js/embed-youtube.js
+++ js/embed-youtube.js
@@ -1,17 +1,17 @@
-document.querySelectorAll(".embed-container").forEach(container => {
-  const youtubeVideoId = container.getAttribute("youtube-video-id");
-  const link = container.querySelector("a");
-  if (youtubeVideoId) {
+const container=document.getElementById("youtubeContainer");
+const youtubeVideoId = container.getAttribute("youtube-video-id");
+const link = container.querySelector("a");
+if (youtubeVideoId) {
     link.addEventListener("click", event => {
-      event.preventDefault();
-      const iframe = document.createElement("iframe");
-      iframe.setAttribute("src", "https://www.youtube-nocookie.com/embed/" + youtubeVideoId + "?autoplay=1");
-      iframe.setAttribute("frameborder", "0");
-      iframe.setAttribute("width", "560");
-      iframe.setAttribute("height", "315");
-      iframe.setAttribute("allow", "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; fullscreen");
-      container.innerHTML = "";
-      container.appendChild(iframe);
+	event.preventDefault();
+	const iframe = document.createElement("iframe");
+	iframe.setAttribute("src", "https://www.youtube-nocookie.com/embed/" + youtubeVideoId + "?autoplay=1");
+	iframe.setAttribute("frameborder", "0");
+	iframe.setAttribute("width", "560");
+	iframe.setAttribute("height", "315");
+	iframe.setAttribute("allow", "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; fullscreen");
+	container.innerHTML = "";
+	container.appendChild(iframe);
     })
-  }
-});
+}
+
