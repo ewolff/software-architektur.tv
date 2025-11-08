1260a7568c6f69a670bbd8c230bb00d4af774f79
Eberhard Wolff <eberhard.wolff@posteo.net>
2025-11-08 10:31:03 +0100

    Disable YouTube autoplay

--- js/embed-youtube.js
+++ js/embed-youtube.js
@@ -5,11 +5,11 @@ const youtubeOriginalContent = youtubeContainer.innerHTML;
 
 function showYoutubePlayer() {
     const iframe = document.createElement("iframe");
-    iframe.setAttribute("src", "https://www.youtube-nocookie.com/embed/" + youtubeVideoId + "?autoplay=1");
+    iframe.setAttribute("src", "https://www.youtube-nocookie.com/embed/" + youtubeVideoId );
     iframe.setAttribute("frameborder", "0");
     iframe.setAttribute("width", "560");
     iframe.setAttribute("height", "315");
-    iframe.setAttribute("allow", "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; fullscreen");
+    iframe.setAttribute("allow", "accelerometer; encrypted-media; gyroscope; picture-in-picture; fullscreen");
     youtubeContainer.innerHTML = "";
     youtubeContainer.appendChild(iframe);
     localStorage.setItem("youtubeConsent","true");
